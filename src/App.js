3f45c4df884e09247e6db7ff71995abf7a6e98c9
Sindhuja-Axim <crazydeveloper.p@gmail.com>
2024-09-19 18:19:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ const App = () => {
   const [selectedOption, setSelectedOption] = useState('Select an option');
   const options = ['Option 1', 'Option 2', 'Option 3'];
 
-
   const handleSelect = (option) => {
     setSelectedOption(option);
   };
-  
+
   return (
     <div>
       <CustomSelect
